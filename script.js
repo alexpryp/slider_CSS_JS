5395d7bc2eb9545981a38ae5a887e453e8960330
alexpryp <alexpryp@gmail.com>
2019-04-23 02:15:43 +0300

    Added animation

--- script.js
+++ script.js
@@ -15,17 +15,69 @@ function slider(selector) {
 			let active = slider.find('.slider__slide_active');
 			let current = active.index();
 			let next = current;
+			let left = false;
 
 			if (a.hasClass('slider__arrow_left')) {
 				next = current - 1 >= 0 ? current - 1 : imgs.length - 1;
+				left = true;
 			} else if (a.hasClass('slider__arrow_right')) {
 				next = (current + 1) % imgs.length;
 			} else {
 				next = a.index();
+				left = next < current ? true : false;
 			}
 
 			if (current == next) {
 				return;
+			}
+
+			slider.append('<div class="slider__temp"></div>');
+
+			let temp = slider.find('.slider__temp');
+			let i = current;
+			let j = 0;
+			let animate = {};
+
+			while (true) {
+				let img = imgs
+					.eq(i)
+					.clone()
+					.css({
+						display: 'inline-block',
+						width: slider.css('width')
+					});
+
+				if (left) {
+					img.prependTo(temp);
+				} else {
+					img.appendTo(temp);
+				}
+
+				if (i == next) {
+					break;
+				}
+
+				if (left) {
+					i = i - 1 >= 0 ? i - 1 : imgs.length - 1;
+					j--
+				} else {
+					i = (i + 1) % imgs.length;
+					j++;
+				}
+			}
+
+			temp.css({
+				width: (Math.abs(j) + 1) * 100 + '%',
+				position: 'absolute',
+				top: 0
+			});
+
+			if (left) {
+				temp.css('left', j * 100 +'%'),
+				animate.left = 0;
+			} else {
+				temp.css('left', 0);
+				animate.left = j * -100 + '%';
 			}
 
 			active.removeClass('slider__slide_active');
@@ -39,6 +91,10 @@ function slider(selector) {
 				.find('.slider__dot')
 				.eq(next)
 				.addClass('slider__dot_active');
+
+			temp.animate(animate, 500, function () {
+				temp.remove();
+			})
 		})
 
 	let slides = slider.children('.slider__slides');
